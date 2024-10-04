6411f6daf328dce5b66cbe77337e5d8420d3b942
Farhan-FMS <61820314+fms-byte@users.noreply.github.com>
2024-10-04 12:23:53 +0600

    Added Landing Page

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -61,7 +61,7 @@ export default function page() {
   });
 
   const updateURL = useCallback(() => {
-    const params = new URLSearchParams(searchParams);
+    const params = new URLSearchParams(searchParams.toString());
     params.set("from", date?.from?.toISOString() || "");
     params.set("to", date?.to?.toISOString() || "");
     params.set("start_hh", start_hh.toString());
--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default async function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       
-      <body className={`${inter.className} overflow-hidden`}>
+      <body className={`${inter.className}`}>
         <Providers session={session}>
           <Toaster />
           
